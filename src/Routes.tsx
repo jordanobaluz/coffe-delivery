05d64ea1234590ee3e49daf0cf3a8c3486221e88
JordanoBaluz <jordanolb95@hotmail.com>
2023-01-02 21:09:20 -0300

    refatorado a rota para trabalhar com o header em todas as pages

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,13 +1,16 @@
 import { Route } from "react-router";
 import { Routes } from "react-router-dom";
+import { DefaultLayout } from "./layouts/DefaultLayout";
 import { CompleteOrderPage } from "./page/CompleteOrder";
-import { HomePage } from "./page/home";
+import { HomePage } from "./page/Home";
 
 export function Router(){
     return (
         <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/completeorder" element={<CompleteOrderPage />} />
+            <Route path="/" element={<DefaultLayout />}>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/completeorder" element={<CompleteOrderPage />} />
+            </Route>
         </Routes>
     )
 }
