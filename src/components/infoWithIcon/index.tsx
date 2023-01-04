6db78eb18a1ec1d38bf1e74297ee1b72beecb976
JordanoBaluz <jordanolb95@hotmail.com>
2023-01-03 21:23:43 -0300

    criado componente que exibe os icones com os textos, passagem por parametro das informações de icon, text e color

---
+++ src/components/infoWithIcon/index.tsx
@@ -0,0 +1,20 @@
+import { ReactNode } from "react";
+import { IconContainer, InfoWithIconContainer } from "./styles";
+
+interface InfoWithIconProps {
+    icon: ReactNode
+    text: string | ReactNode // pode ser tanto texto como HTML
+    iconBg: string
+}
+
+export function InfoWithIcon({icon, text, iconBg}: InfoWithIconProps){
+    return (
+        <InfoWithIconContainer>
+            <IconContainer iconBg={iconBg}>
+                {icon}
+            </IconContainer>
+            {/* se for mesmo string, mostra mostra o p se não é um HTML */}
+            {typeof text === 'string' ? <p>{text}</p> : text}
+        </InfoWithIconContainer>
+    )
+}
