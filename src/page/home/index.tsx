afca2eca3352d4faf2a5b2cf1887c73b2fb14df5
JordanoBaluz <jordanolb95@hotmail.com>
2023-01-03 21:52:25 -0300

    importado e inserido o componente para exibir os cafés com o componente OurCoffees

--- src/page/home/index.tsx
+++ src/page/home/index.tsx
@@ -1,11 +1,13 @@
-import { Intro } from "./components/intro";
-import { IntroContainer } from "./components/intro/styles";
+import { Intro } from "./components/Intro";
+import { IntroContainer } from "./components/Intro/styles";
+import { OurCoffees } from "./components/OurCoffees";
 import { HomeContainer } from "./styles";
 
 export function HomePage(){
     return (
         <HomeContainer>
             <Intro />
+            <OurCoffees />
         </HomeContainer>
     )
 }
