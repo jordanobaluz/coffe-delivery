0879218bcaaab679db6a790946d412c5ed348db1
JordanoBaluz <jordanolb95@hotmail.com>
2023-01-03 21:24:31 -0300

    importado componente de InfoIconContainer para exibir na home as informações sobre o produto com os ícones

--- src/page/home/components/intro/styles.ts
+++ src/page/home/components/intro/styles.ts
@@ -32,3 +32,11 @@ gap: 3.5rem;
 export const IntroTitle = styled(TitleText)`
     margin-bottom: 1rem;
 `
+
+export const BenefitsContainer = styled.div`
+  width: 100%;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  row-gap: 1.25rem;
+  margin-top: 4.125rem;
+`
