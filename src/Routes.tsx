7ba95bad0df41e0448d82d59931f88663636a01b
JordanoBaluz <jordanolb95@hotmail.com>
2023-01-09 20:32:19 -0300

    Adicionado imagem de confirmação do pedido. Adicionado componente de confirmação de pedido nas rotas

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -3,6 +3,7 @@ import { Routes } from "react-router-dom";
 import { DefaultLayout } from "./layouts/DefaultLayout";
 import { CompleteOrderPage } from "./page/CompleteOrder";
 import { HomePage } from "./page/Home";
+import { OrderConfirmedPage } from "./page/OrderComfirmed";
 
 export function Router(){
     return (
@@ -10,6 +11,7 @@ export function Router(){
             <Route path="/" element={<DefaultLayout />}>
                 <Route path="/" element={<HomePage />} />
                 <Route path="/completeorder" element={<CompleteOrderPage />} />
+                <Route path="/orderConfirmed" element={<OrderConfirmedPage />} />
             </Route>
         </Routes>
     )
