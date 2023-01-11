c975bbde0aac0c19d89647655647a517212b91da
JordanoBaluz <jordanolb95@hotmail.com>
2023-01-10 22:24:14 -0300

    refatorado para exibir os dados do pedido feito que foi pego via contexto e refatorado para não quebrar a pagina quando acessar a url

--- src/page/OrderComfirmed/index.tsx
+++ src/page/OrderComfirmed/index.tsx
@@ -4,9 +4,31 @@ import confirmedOrderIlustration from "../../assets/confirmed-order.svg"
 import { InfoWithIcon } from "../../components/infoWithIcon";
 import { Clock, CurrencyDollar, MapPin } from "phosphor-react";
 import { useTheme } from "styled-components";
+import { useLocation, useNavigate } from "react-router-dom";
+import { OrderData } from "../CompleteOrder";
+import { paymentMethods } from "../CompleteOrder/components/CompleteOrderForm/PaymentMethodOptions";
+import { useEffect } from "react";
+
+interface LocationType{
+    state: OrderData;
+}
 
 export function OrderConfirmedPage(){
-    const { colors } = useTheme()
+    const { colors } = useTheme();
+
+    const { state } = useLocation() as unknown as LocationType;
+    
+    const navigate = useNavigate();
+
+    useEffect(
+        () => {
+            if(!state){
+                navigate("/")
+            }
+        }, [])
+
+    // retorna html vazio para não quebrar a pagina
+    if(!state) return <></>
 
     return(
         <OderConfirmedContainer className="container">
@@ -23,9 +45,9 @@ export function OrderConfirmedPage(){
                         iconBg={colors["brand-purple"]}
                         text={
                         <RegularText>
-                            Entrega em <strong>Rua João Daniel, 102</strong>
+                            Entrega em <strong>{state.street}, {state.number}</strong>
                             <br />
-                            Farrapos - Porto Alegre, RS
+                            {state.district} - {state.city}, {state.uf}
                         </RegularText>}
                     />
                     <InfoWithIcon 
@@ -45,7 +67,7 @@ export function OrderConfirmedPage(){
                         <RegularText>
                             Pagamento na entrega
                             <br />
-                            <strong>Cartão de Crédito</strong>
+                            <strong>{paymentMethods[state.paymentMethod].label}</strong>
                         </RegularText>}
                     />
                 </OrderDetailsContainer>
